6c5e981ce6f63adace54ec006ad849fbea9921f9
Jieun Rim <rim.jieun@gmail.com>
2017-03-11 07:24:32 -0500

    ready for submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,68 @@
 $(document).ready(function() {
 
+  var instructions1 = $("#instructions1");
+  var instructions2 = $("#instructions2");
+  gifSection = $("#gif-section");
+
+  instructions2.hide();
+
   var topicList = [
-    "Lost",
-    "Newsroom",
-    "Friends",
-    "How I Met Your Mother",
-    "Jane the Virgin",
-    "Game of Thrones"
+    "dog",
+    "developer",
+    "universe",
+    "i am hungry",
+    "matrix"
   ]
 
   // Create a topic buttons.---------------------------------------------------------
   function createBtns() {
     for (i = 0; i < topicList.length; i++) {
-      var button = $("<button class='topicBtn' value='" + topicList[i] + "'>" + topicList[i] + "</button>")
+
+      var button = $("<li>");
+      button.addClass("topicBtn")
+            .attr("value", topicList[i])
+            .text(topicList[i]);
+
+      var deleteIcon = $("<i class='material-icons'>cancel</i>")
+      deleteIcon.attr("value", topicList[i]);
+
+      button.append(deleteIcon);
       $("#btns-section").append(button);
     }
   }
-  //----------------------------------------------------------------------------------
+ 
 
   createBtns();
 
   // When you click "Add", add a button to buttons list.------------------------------
   $("#add-btn").on("click", function(event) {
-    $("#btns-section").empty();
     event.preventDefault();
-    var newTopic = $("input").val();
-    topicList.push(newTopic);
-    console.log(topicList);
-    createBtns();
-  });
-  //-----------------------------------------------------------------------------------
 
-  // When you click a topic button, output 10 gifs for that topic.---------------------
+    
+    $("#btns-section").empty();
+
+    var newTopic = $("#user-input").val();
+
+    if ((newTopic !== "") && (topicList.indexOf(newTopic) === -1)) {
+      topicList.push(newTopic);
+    }
+
+    $("#user-input").val("");
+
+    createBtns();
+    
+  });
+  
+
+  // When user clicks a topic button, output 10 gifs for that topic.---------------------
   $(document).on("click", ".topicBtn", function() {
 
-    $("#results").empty();
+    instructions1.hide();
+    instructions2.show();
 
-    var topic = $(this).val();
+    var topic = $(this).attr("value");
     var key = "dc6zaTOxFJmzC";
-    var limit = 10;
+    var limit = 100;
     var url = "http://api.giphy.com/v1/gifs/search";
     url += "?" + $.param({
       "api_key": key,
@@ -51,27 +75,47 @@ $(document).ready(function() {
       method: "GET"
     }).done(function(response) {
 
-      for (i = 0; i < response.data.length; i++) {
+      gifSection.empty();
 
-        var animateURL = response.data[i].images.fixed_height.url;
-        var stillURL = response.data[i].images.fixed_height_still.url;
+      var gifIndexArray = []; // For randomizing gifs
 
-        var newImg = $("<img>");
-        newImg.attr("src", animateURL)
+      for (i = 0; i < 10; i++) {
+        var index = Math.floor(Math.random() * 100);
+        gifIndexArray.push(index);
+      }
+
+      for (i = 0; i < gifIndexArray.length; i++) {
+
+        var gifIndex = gifIndexArray[i];
+
+        var originalURL = response.data[gifIndex].images.original.url;
+        var animateURL = response.data[gifIndex].images.fixed_height.url;
+        var stillURL = response.data[gifIndex].images.fixed_height_still.url;
+        var rating = response.data[gifIndex].rating.toUpperCase();
+
+        var newDiv = $("<div class='gifDiv'>");
+
+        var ratingDiv = $("<p class='ratingDiv'>")
+        ratingDiv.text("Rating: " + rating);
+
+        var imgDiv = $("<img>");
+        imgDiv.attr("src", stillURL)
         .attr("data-still", stillURL)
         .attr("data-animate", animateURL)
-        .attr("data-state", "animate");
+        .attr("data-state", "still")
+        .attr("data-original", originalURL);
 
-        $("#results").append(newImg);
+        newDiv.append(imgDiv);
+        newDiv.append(ratingDiv);
+        gifSection.append(newDiv);
       }
     }).fail(function(err) {
       throw err;
     });
   });
-  //---------------------------------------------------------------------------------
+  
 
-
-  // When you click a gif, stop or animate it.---------------------------------------
+  // When user click a gif, stop or animate it.---------------------------------------
   $(document).on("click", "img", function() {
 
     var state = $(this).attr("data-state");
@@ -86,8 +130,21 @@ $(document).ready(function() {
       $(this).attr("data-state", "still");
       $(this).attr("src", still);  
     }
-  });//---------------------------------------------------------------------------------
+  });
+  
 
+  // When user double clicks gif, prompt gif url.----------------------------------------
+  $(document).on("dblclick", "img", function() {
+    var original = $(this).attr("data-original");
+    prompt("GIF URL:", original);
+  });
+  
 
+  // When user clicks x, remove button.----------------------------------------------
+  $(document).on("click", ".material-icons", function() {
+    var deleteValue = $(this).attr("value");
+    $("li[value='" + deleteValue + "']").remove();
+    topicList.splice($.inArray(deleteValue, topicList), 1);
+  });
 
 });
